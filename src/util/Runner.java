071aa663fa2f9d7dae3bd43c48e3a8bcd39739d2
Aaron Jenner <ajenner@guidewire.com>
2023-12-03 15:45:55 +0000

    Day3 solutions

--- src/util/Runner.java
+++ src/util/Runner.java
@@ -2,13 +2,15 @@ package util;
 
 import day1.Day1;
 import day2.Day2;
+import day3.Day3;
 
 import java.util.ArrayList;
 
 public class Runner {
     public static void main(String[] args) {
-        runDay1();
-        runDay2();
+        //runDay1();
+        //runDay2();
+        runDay3();
     }
 
     private static void runDay1() {
@@ -25,5 +27,13 @@ public class Runner {
         ArrayList<String> day2Inputs = reader.readAsStrings();
         System.out.println("Question 1: " + Day2.part1(day2Inputs));
         System.out.println("Question 2: " + Day2.part2(day2Inputs));
+    }
+
+    private static void runDay3() {
+        System.out.println("Day3: ");
+        Reader reader = new Reader("src/data/day3.txt");
+        ArrayList<String> day3Inputs = reader.readAsStrings();
+        System.out.println("Question 1: " + Day3.part1(day3Inputs));
+        System.out.println("Question 2: " + Day3.part2(day3Inputs));
     }
 }
